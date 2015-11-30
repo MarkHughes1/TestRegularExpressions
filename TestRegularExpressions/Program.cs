4e9209bebf00b35e6c35bbd5102ab506483772d4
Mark Hughes <mark.hughes4096@gmail.com>
2015-11-30 19:41:01 +0000

    test

--- TestRegularExpressions/Program.cs
+++ TestRegularExpressions/Program.cs
@@ -6,10 +6,38 @@ using System.Threading.Tasks;
 
 namespace TestRegularExpressions
 {
-    class Program
+    class TestRegularExpressions
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            string[] sentences =
+            {
+            "C# code",
+            "Chapter 2: Writing Code",
+            "Unicode",
+            "no match here"
+        };
+
+            string sPattern = "code";
+
+            foreach (string s in sentences)
+            {
+                System.Console.Write("{0,24}", s);
+
+                if (System.Text.RegularExpressions.Regex.IsMatch(s, sPattern, System.Text.RegularExpressions.RegexOptions.IgnoreCase))
+                {
+                    System.Console.WriteLine("  (match for '{0}' found)", sPattern);
+                }
+                else
+                {
+                    System.Console.WriteLine();
+                }
+            }
+
+            // Keep the console window open in debug mode.
+            System.Console.WriteLine("Press any key to exit.");
+            System.Console.ReadKey();
+
         }
     }
 }
